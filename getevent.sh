28dfdafae13b795f0311156ef98db00a308a8f69
mattak <mattak.me@gmail.com>
2019-01-21 18:04:04 +0900

    :seedling: Rename cathand-exe -> cathand-runner

--- getevent.sh
+++ getevent.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# [  128893.610543] /dev/input/event2: 0000 0000 00000000
 
 adb shell getevent -t | egrep --line-buffered '^\[' | perl -ne '$|=1; $_ =~ s|\[\s*(\d+\.\d+)\s*\]\s+\S+\s+|\1 |g; print $_' 
-
