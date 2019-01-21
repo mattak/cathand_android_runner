71d92ebb9fdb0a26b15367a7980787ed9e84d4c9
mattak <mattak.me@gmail.com>
2019-01-21 17:44:33 +0900

    :seedling: Fix install scripts

--- copy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rsync -av app/build/intermediates/cmake/debug/obj/ app/src/main/assets/
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-ARCH=$(adb shell getprop ro.product.cpu.abi)
-adb push ./app/build/intermediates/cmake/debug/obj/$ARCH/cathand.so /data/local/tmp/cathand
-adb shell chmod +x /data/local/tmp/cathand
-adb shell /data/local/tmp/cathand
+ARCH=$(adb shell getprop ro.product.cpu.abi | perl -pne 's|\r||g')
+SRC_FILE=./app/build/intermediates/cmake/debug/obj/$ARCH/cathand.so
+OUT_FILE=/data/local/tmp/cathand
+adb push $SRC_FILE $OUT_FILE
+adb shell chmod +x $OUT_FILE
+echo adb push /tmp/getevent /data/local/tmp/getevent.tsv
+echo adb shell /data/local/tmp/cathand /dev/input/event2 /data/local/tmp/getevent.tsv
